b30da365db11ffe9b0b1d6a8b871fd8969ca0791
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2025-11-10 19:31:02 +0100

    Update Test Project

---
+++ MimeTypesTest/Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace MimeTypesTest;
+
+[TestClass]
+public sealed class Tests
+{
+    [TestMethod]
+    public void MimeTypesTest()
+    {
+        var mp4 = MimeTypes.MimeTypes.GetMimeTypes("test.mp4");
+        Assert.IsTrue(mp4.Contains("video/mp4"));
+        Assert.AreEqual(1, mp4.Count());
+
+
+        var ogg = MimeTypes.MimeTypes.GetMimeTypes("test.ogg");
+        Assert.IsTrue(ogg.Contains("video/ogg"));
+        Assert.IsTrue(ogg.Contains("audio/ogg"));
+
+        Assert.AreEqual(2, ogg.Count());
+    }
+}
